9bdd66c11a557d42a86ac85dac8c478b5db354c2
Yonghee <yhlee73@gmail.com>
2014-05-08 16:07:58 +0900

    aa
    :wq

---
+++ lodaDataFile.R
@@ -0,0 +1,25 @@
+loadDataFile <- function(){
+    library(plyr)
+    fileName <- "./data/household_power_consumption.txt"
+    rawData <- read.csv(file = fileName, sep=";", header=T,stringsAsFactors=F)
+    
+    # subset data 
+    resultData <- subset(rawData, Date=="1/2/2007" | Date=="2/2/2007")
+    
+    # numeric column cleaning
+    for(colId in 3:9) {
+        resultData[,colId] <-  gsub("\\?",NA, resultData[,colId])
+        resultData[,colId] <- as.numeric(resultData[,colId])
+    }
+    
+    #Date and Time column convert to Date/Column classes.
+    tmpVect <-  apply(as.data.frame(resultData[1:2],stringsAsFactors=F),1, paste, collapse=" ")
+    timeVect <-  strptime(tmpVect,"%d/%m/%Y %H:%M:%S")
+    resultData$Date <- as.Date(resultData$Date, format="%d/%m/%Y")
+    
+    #finaly getting tidy date set
+    resultData <- cbind(timeVect,resultData)
+    colnames(resultData)[1] <- "DateTime"
+    rownames(resultData) <- NULL
+    return(resultData)
+}
