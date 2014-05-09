b47b198e29d3ddebc4f95264a3ac6630afadabe9
Yonghee <yhlee73@gmail.com>
2014-05-10 01:52:54 +0900

    fix plot3 legend

--- plot3.R
+++ plot3.R
@@ -8,6 +8,6 @@ par(mfrow=c(1,1))
 plot(myData$DateTime,myData$Sub_metering_1,type="l", xlab="" , ylab="Energy Sub Metering")
 lines(myData$DateTime,myData$Sub_metering_2,type="l",col="red")
 lines(myData$DateTime,myData$Sub_metering_3,type="l",col="blue")
-legend("topright",c("sub_metering_1","sub_metering_2","sub_metering_1"),col=c("black","red","blue"),lty="solid")
+legend("topright",c("sub_metering_1","sub_metering_2","sub_metering_3"),col=c("black","red","blue"),lty="solid")
 
 dev.off()
